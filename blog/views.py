0a07c66393eac612ce3524d958901daac00c74e3
alxhghs <alxhghs@gmail.com>
2017-04-08 10:59:48 -0400

    added links to individual posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date') # lte = less than or equal to
-    return render(request, 'blog/post_list.html', {'posts': posts}) # request = everything we receive from the user via the internet; url is the template file; {} = things for the template to use 
+    return render(request, 'blog/post_list.html', {'posts': posts}) # request = everything we receive from the user via the internet; url is the template file; {} = things for the template to use
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
